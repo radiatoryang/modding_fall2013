e02735e1f914806f75544cfa438bb615f9faeef0
Robert Yang <campaignjunkie@gmail.com>
2013-11-14 11:06:41 -0500

    added sheep fame particles

---
+++ modding_week10-15/Assets/scripts/PlayerInput.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour {
+
+    public Sheep dolly; // assign this PREFAB in inspector
+    public TextMesh fameMeter; // assign this reference in inspector
+
+    // "static" means this variable doesn't live in the scene
+    // it means that this variable lives inside the code itself
+    // and if we combine this with "public" then ANY PIECE OF CODE ANYWHERE can access the var!
+    //      e.g. if we made 20 copies of PlayerInput on 20 gameObjects
+    //      then they would all still only reference the SAME "selectedSheep"
+    //      because it is *static*, it lives in the class definition itself
+    public static Sheep currentSheep;
+
+    // the reason this is USEFUL: you no longer have to drag and drop anything
+    // and you don't have to use GetComponent because this variable lives in code!
+
+    // the reason this is TERRIBLE: anything in the code can read and write to this var
+    // which means a lot of complexity; if something goes wrong, it could be ANYWHERE
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+        RaycastHit rayHit = new RaycastHit();
+
+        if ( Physics.Raycast( ray, out rayHit, 10000f ) ) {
+
+            if ( Input.GetMouseButtonDown( 0 ) && rayHit.collider.tag == "Sheep" ) {
+                // whatever code runs here is when:
+                // 1) the mouse is over a GameObject with a collider and "Sheep" tag
+                // 2) AND when the mouse is left-clicked
+                Sheep selectedSheep = rayHit.collider.GetComponent<Sheep>();
+                fameMeter.text = "sheep fame = " + selectedSheep.fame.ToString();
+
+                // added on 11/14/2013
+                currentSheep = selectedSheep; // saves what sheep we clicked on
+            }
+
+            if ( Input.GetMouseButtonDown( 1 ) ) {
+                // whatever code runs here is when:
+                // 1) the mouse is over anything with a collider
+                // 2) AND when the mouse is right-clicked
+                Sheep newSheep = Instantiate( dolly, rayHit.point, Quaternion.identity ) as Sheep;
+                newSheep.fame = Random.Range( 1, 100 );
+            }
+
+        } // this closes out Raycast()
+
+
+	} // this close out Update()
+
+}
---
+++ modding_week10-15/Assets/scripts/Sheep.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sheep : MonoBehaviour {
+
+    // "public" does 2 things:
+    // access in the editor, on per-sheep basis
+    // it lets other scripts access this variable
+    public int fame = 10;
+
+    // first I need a reference to the particle system component
+    public ParticleSystem fameParticles; // assign this in inspector
+
+    void Start() {
+        // if you put ParticleSystem with "AddComponent" on the same sheep obj
+        // then you would GetComponent<>() instead
+        // fameParticles = GetComponent<ParticleSystem>();
+
+        fameParticles.emissionRate = fame;
+    }
+
+    void Update() {
+        // if we are the current sheep, enable particles; otherwise, turn off
+        if ( PlayerInput.currentSheep == this ) {
+            fameParticles.enableEmission = true;
+        } else {
+            fameParticles.enableEmission = false;
+        }
+
+    }
+
+}
