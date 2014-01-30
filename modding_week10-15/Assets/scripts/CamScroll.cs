aae9f652ba12608be93fcce39b3f01fe441450fa
Robert Yang <campaignjunkie@gmail.com>
2014-01-29 23:54:34 -0500

    last commit, bump map stuff / shader stuff

---
+++ modding_week10-15/Assets/scripts/CamScroll.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamScroll : MonoBehaviour {
+	float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			transform.position += Input.GetAxis ("Horizontal") * Vector3.right * Time.deltaTime * speed;
+			transform.position += Input.GetAxis ("Vertical") * Vector3.forward * Time.deltaTime * speed;
+	}
+}
